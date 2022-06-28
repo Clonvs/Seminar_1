1aa8dc702b3c323d41df428c6e30c6f177239541
igor <igor.borodin.888@gmail.com>
2022-06-28 13:59:38 +0300

    task

---
+++ sem001/Program.cs
@@ -0,0 +1,15 @@
+﻿//Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+Console.Write("Input first number: ");
+int number_1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input second number: ");
+int number_2 = Convert.ToInt32(Console.ReadLine());
+
+if (number_1 > number_2)
+    Console.Write ("a = " + number_1 + ", b = "+ number_2 + " ->" + " a");
+else 
+    Console.WriteLine("a = " + number_1 + ", b = "+ number_2 + " -> "+ " b");   
+
+
+//Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел
+
